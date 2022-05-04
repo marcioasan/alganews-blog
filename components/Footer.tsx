34483e3cd3e4c5bad6549c9587e340858dfdae40
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-05-04 15:15:49 -0300

    11.12. Criando o Layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,30 +1,32 @@
+//11.12. Criando o Layout
 import styled from "styled-components";
-import { transparentize } from "polished"
-import { HEADER_HEIGHT } from "../_constants";
+import { transparentize } from "polished";
+import { FOOTER_HEIGHT } from "../_constants";
 
-export default function Header(props: any) {
+export default function Footer(props: any) {
   return (
     <Wrapper>
       <Container>
         <span>logo</span>
-        <span>navbar</span>
+        <span>créditos</span>
       </Container>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.footer`
   background-color: ${(p) => p.theme.activeElementBackground};
   color: ${(p) => p.theme.activeElementForeground};
-  box-shadow: 0 3px 10px ${(p) => transparentize(0.9, p.theme.pageForeground)};
   width: 100%;
-  height: ${HEADER_HEIGHT}px;
+  height: ${FOOTER_HEIGHT}px;
 `;
 
 const Container = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
+  flex-direction: column;
+  gap: 16px;
   max-width: 848px;
   margin: auto;
   height: 100%;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -14,7 +13,6 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <Header />
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
@@ -53,20 +51,7 @@ const Home: NextPage = () => {
             </p>
           </a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      </main>      
     </div>
   )
 }
