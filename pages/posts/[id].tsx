4a93dee75867754ce728020d5200bf262c0e5395
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-05-19 20:21:23 -0300

    11.32. Introdução à tratativa de erros com Next

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,14 +1,14 @@
-//11.31. Parâmetros nas rotas - 2'
+//11.31. Parâmetros nas rotas - 2', 11.32. Introdução à tratativa de erros com Next
 import { Post, PostService } from "marcioasan-sdk";
 import { GetServerSideProps } from "next";
 import { ParsedUrlQuery } from "querystring";
 
 interface PostProps {
-  post: Post.Datailed;
+  post?: Post.Datailed;
 }
 
 export default function PostPage(props: PostProps) {
-  return <div>{props.post.title}</div>;
+  return <div>{props.post?.title}</div>;
 }
 
 interface Params extends ParsedUrlQuery {
@@ -17,18 +17,24 @@ interface Params extends ParsedUrlQuery {
 
 export const getServerSideProps: GetServerSideProps<PostProps, Params> =
   async ({ params }) => {
-    if (!params) return { notFound: true };
+    try {
+      if (!params) return { notFound: true };
 
-    const { id } = params;
-    const postId = Number(id);
+      const { id } = params;
+      const postId = Number(id);
 
-    if (isNaN(postId)) return { notFound: true };
+      if (isNaN(postId)) return { notFound: true };
 
-    const post = await PostService.getExistingPost(postId);
+      const post = await PostService.getExistingPost(postId);
 
-    return {
-      props: {
-        post,
-      },
-    };
+      return {
+        props: {
+          post,
+        },
+      };
+    } catch (err) {
+      return {
+        props: {},
+      };
+    }
   };
