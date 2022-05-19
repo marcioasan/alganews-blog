4ca4dee81448216f38c1100ac33f9a0f9993f3de
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-05-19 20:13:31 -0300

    11.31. Parâmetros nas rotas

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,34 @@
+//11.31. Parâmetros nas rotas - 2'
+import { Post, PostService } from "marcioasan-sdk";
+import { GetServerSideProps } from "next";
+import { ParsedUrlQuery } from "querystring";
+
+interface PostProps {
+  post: Post.Datailed;
+}
+
+export default function PostPage(props: PostProps) {
+  return <div>{props.post.title}</div>;
+}
+
+interface Params extends ParsedUrlQuery {
+  id: string;
+}
+
+export const getServerSideProps: GetServerSideProps<PostProps, Params> =
+  async ({ params }) => {
+    if (!params) return { notFound: true };
+
+    const { id } = params;
+    const postId = Number(id);
+
+    if (isNaN(postId)) return { notFound: true };
+
+    const post = await PostService.getExistingPost(postId);
+
+    return {
+      props: {
+        post,
+      },
+    };
+  };
