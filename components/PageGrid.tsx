07bf036c7a9f44ce282203f757403a08a14289e2
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-05-19 18:00:55 -0300

    11.28. Deixando a lista responsiva

---
+++ components/PageGrid.tsx
@@ -0,0 +1,8 @@
+//11.28. Deixando a lista responsiva
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
--- components/PostsGrid.tsx
+++ components/PostsGrid.tsx
@@ -3,6 +3,11 @@ import styled from "styled-components";
 
 export default styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  //grid-template-columns: repeat(3, 1fr);
   gap: 16px;
+
+  //11.28. Deixando a lista responsiva - 2'20"
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 `;
