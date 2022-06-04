535ca47128b194c76d84fe05a72e8b1c6861a071
Marcio Antonio dos Santos <marcioasan@gmail.com>
2022-06-04 17:08:06 -0300

    11.50. Barra de progresso

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,8 @@ import GlobalStyles from '../styles/globalStyles';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Content from '../components/Content';
-import { pathToFileURL } from 'url'
+import ProgressBar from "@badrap/bar-of-progress"
+import { Router } from 'next/router'
 
 //11.34. Reutilizando a lógica de renderização de erro - 2'
 interface CustomAppProps extends NextPageProps {}
@@ -16,6 +17,13 @@ interface CustomAppProps extends NextPageProps {}
 type AppProps<P = any> = {
   pageProps: P
 } & Omit<NextAppProps<P>, "pageProps">
+
+//11.50. Barra de progresso - 6'
+const progress = new ProgressBar({
+  size: 2,
+  color: light.primaryBackground,
+  delay: 100,
+});
 
 function MyApp({ Component, pageProps }: AppProps<CustomAppProps>) {
   
@@ -40,5 +48,10 @@ function MyApp({ Component, pageProps }: AppProps<CustomAppProps>) {
   </ThemeProvider>
   );
 }
+
+//11.50. Barra de progresso - 6'20"
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 export default MyApp
